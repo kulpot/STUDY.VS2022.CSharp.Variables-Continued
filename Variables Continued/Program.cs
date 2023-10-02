ca92665bd1af174908cd833fab44d9508b76c388
Kulpot <kulpotwahu@gmail.com>
2023-10-02 19:22:28 +0800

    Add project files.

---
+++ Variables Continued/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=WtylRM5bxoM&list=PLF932FF551E0ADD16&index=5
+//
+
+class MyClass
+{
+    static void Main()
+    {
+        //Console.WriteLine("Kulpot Wahu");
+        string myString = "Kulpot Wahu";
+        int age = 30;
+        //age = "Kulpot Wahu";
+        //String = 30;
+        Console.WriteLine(myString);
+        Console.WriteLine(age);
+
+    }
+}
